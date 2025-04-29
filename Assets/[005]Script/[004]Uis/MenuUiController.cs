e0e66db495ad327e21a2e4bf421b11e5b7247398
ryu8655-alt <suzukiriyuuji@gmail.com>
2025-04-30 02:10:46 +0900

    DOTweenのインポートをしました

---
+++ Assets/[005]Script/[004]Uis/MenuUiController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+///  タイトル画面でのUI(Menuボタンらと背景画像の演出関係を担うスクリプト
+/// </summary>
+public class MenuUiController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// 開始地点のポジションから終点のポジションまでスライド移動してくる
+    /// メンバー変数を扱うためインスペクター上で設定可能にする
+    /// </summary>
+    private void SlideMenuUis()
+    {
+
+    }
+}

45650d30b03695480c39d656f16004c10ba75813
ryu8655-alt <suzukiriyuuji@gmail.com>
2025-05-23 10:37:29 +0900

    武器の追加：ナイフ

--- Assets/[005]Script/[003]Weapon/TestWeapons/Knife/KnifeController.cs
+++ Assets/[005]Script/[003]Weapon/TestWeapons/Knife/KnifeController.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KnifeController : MonoBehaviour
+public class KnifeController : BaseWeapon
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Šp“x‚Ì•ÏŠ·‚ğs‚¤
+        float angle = Mathf.Atan2(_forward.y, _forward.x) * Mathf.Rad2Deg;
+        //Šp“x‚ğ‘ã“ü
+        transform.rotation = Quaternion.Euler(0, 0, angle);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _rigidbody2d.position += _forward.normalized * _weaponStatus.Speed * Time.deltaTime;
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        AttackEnemy(collision);
     }
 }
--- Assets/[005]Script/[003]Weapon/TestWeapons/Knife/KnifeSpawnerController.cs
+++ Assets/[005]Script/[003]Weapon/TestWeapons/Knife/KnifeSpawnerController.cs
@@ -2,17 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KnifeSpawnerController : MonoBehaviour
+public class KnifeSpawnerController : BaseWeaponSpawner
 {
+    //生成に時差をつける
+    int _onceSpawnCount;
+    float _onceSpawnTime = 0.1f;
+    PlayerController _playerController;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _onceSpawnCount = (int) _weaponStatus.SpawnCount;
+        _playerController = transform.parent.GetComponent<PlayerController>();
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isSpawnTimerNotElapsed()) return;
+
+        //武器の生成処理
+        KnifeController ctrl = (KnifeController)CreateWeapon(transform.position, _playerController._forward);
+
+        //次の生成タイマー
+        _spawnTimer = _onceSpawnTime;
+        _onceSpawnCount--;
+
+        //1回の生成が終わったらリセット
+        if(1 > _onceSpawnCount)
+        {
+            _totalDamage = GetRandomSpawnTimer();
+            _onceSpawnCount = (int)_weaponStatus.SpawnCount;
+        }
+
     }
 }
